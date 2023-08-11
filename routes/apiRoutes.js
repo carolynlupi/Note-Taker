59470ed4855d44d27d3ea929381d5c38eaa3b5be
carolynlupi <karolynlupi@gmail.com>
2023-08-10 20:25:40 -0400

    Added in notes to clean up code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-// dependencies 
+// adding in dependencies 
 const path = require('path');
 const fs = require('fs')
 
@@ -7,39 +7,29 @@ var uniqid = require('uniqid');
 
 // routing
 module.exports = (app) => {
-
-    // GET /api/notes should read the db.json file and return all saved notes as JSON.
+    // read the db.json file and return all saved notes as JSON.
     app.get('/notes', (req, res) => {
         let db = fs.readFileSync('db/db.json', 'utf8');
-        console.log('Read DB:', db); // Check the contents of db
+        console.log('Read DB:', db);
         db = JSON.parse(db);
-        // ...
     });
 
-    // POST /api/notes should receive a new note to save on the request body, 
+    // receive a new note to save on the request body, 
     // add it to the db.json file, and then return the new note to the client. 
     app.post('/notes', (req, res) => {
         let db = fs.readFileSync('db/db.json');
         db = JSON.parse(db);
-
-        // creating body for note
         let userNote = {
             title: req.body.title,
             text: req.body.text,
-            // creating unique id for each note
             id: uniqid(),
         };
-        // pushing created note to be written in the db.json file
         db.push(userNote);
         fs.writeFileSync('db/db.json', JSON.stringify(db));
-
-        // Sending the response after adding the note
         res.json(userNote);
     });
 
-
-
-    // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
+    // receive a query parameter containing the id of a note to delete.
     app.delete('/notes/:id', (req, res) => {
         // reading notes from db.json
         let db = JSON.parse(fs.readFileSync('db/db.json'));
@@ -47,7 +37,6 @@ module.exports = (app) => {
         let deleteNotes = db.filter(item => item.id !== req.params.id);
         // Rewriting notes to db.json
         fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-
         // Sending the response after deleting the note
         res.json(deleteNotes);
     });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,10 @@
-// dependencies
+// adding in dependencies
 const path = require('path');
 
 // routing
 module.exports = (app) => {
   // creating routes
-  // GET /notes should return the notes.html file.
+  // return the notes.html file.
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
